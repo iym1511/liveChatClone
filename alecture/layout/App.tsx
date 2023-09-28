5586264a082d83611e754432a503464de25c48d2
문일윤 <mun-il-yun@mun-il-yun-ui-MacBookAir.local>
2023-09-28 23:26:54 +0900

    FEAT Login,SginUp / CustomHooks

--- alecture/layout/App.tsx
+++ alecture/layout/App.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React from 'react';
+import loadable from '@loadable/component';
+import { Redirect, Route, Switch } from 'react-router';
+// "현재 페이지를 렌더링 하는 데에 필요한 코드만 로드되도록"
+// -> 빠른 초기 로딩 속도
+// -> 더 나은 사용자 경험으로 이어짐
+// Loadable Components는 코드 스플리팅을 편하게 하도록 도와주는 서드파티 라이브러리입니다. 이 라이브러리의 이점은 서버 사이드 렌더링을 지원한다는 것입니다
+const LogIn = loadable(() => import('@pages/Login'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
+// Switch 여러개 라우터중 딱 하나만 화면에 표시해주는 것
+// 라우터 중 하나의 페이지만 들어가면 나머지는 없는 셈이 된다.
 const App = () => {
-  return (  
-    <div>핫 리로딩</div>
+  return (
+    <Switch>
+      <Redirect exact path='/' to="/login"/>
+      <Route path="/login" component={LogIn}/>
+      <Route path="/sginup" component={SignUp}/>
+    </Switch>
   );
-}
+};
 
 export default App;
---
+++ alecture/pages/SignUp/index.tsx
@@ -0,0 +1,86 @@
+import React, { useCallback, useState } from "react";
+import { Button, Error, Form, Header, Input, Label, LinkContainer } from "./styles";
+import { Link } from "react-router-dom";
+import useInput from "@hooks/useInput";
+
+
+const SignUp = () => {
+  
+  const [email, onChangeEmail, setEmail] = useInput('');
+  const [nickname, onChangeNickname] = useInput('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
+
+
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+    setMismatchError(e.target.value !== passwordCheck);
+  },[passwordCheck]);
+
+  const onChangePasswordCheck = useCallback((e) => {
+    setPasswordCheck(e.target.value);
+    // 비밀번호 확인 check
+    setMismatchError(e.target.value !== password);
+    console.log(mismatchError)
+  },[password]);
+
+  const onSubmit = useCallback((e) => {
+    e.preventDefault();
+    console.log(email, nickname, password, passwordCheck);
+    setEmail("");
+    if(!mismatchError && nickname){
+      console.log('서버로 회원가입하기');
+    }
+  },[email, nickname, password, passwordCheck]);
+  
+
+  return (  
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input type="email" id="email" name="email" value={email} onChange={onChangeEmail} />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input type="text" id="nickname" name="nickname" value={nickname} onChange={onChangeNickname} />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input type="password" id="password" name="password" value={password} onChange={onChangePassword} />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+          {!nickname && <Error>닉네임을 입력해주세요.</Error>}
+          {/* {signUpError && <Error>{signUpError}</Error>} */}
+          {/* {signUpSuccess && <Success>회원가입되었습니다! 로그인해주세요.</Success>}  */}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <Link to="/login">로그인 하러가기</Link>
+      </LinkContainer>
+      </div>
+  );
+}
+
+export default SignUp;
